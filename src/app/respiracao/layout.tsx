627853ab805737a4fac7b416c0a566206cb08f51
Leticia Eltermann <140015770+leticiael@users.noreply.github.com>
2025-06-18 17:53:09 -0300

    fix zen mode

--- src/app/respiracao/layout.tsx
+++ src/app/respiracao/layout.tsx
@@ -1,10 +1,15 @@
+"use client";
 import FloatingAudioPlayer from "@/components/FloatingAudioPlayer";
+import { ZenModeContext } from "@/components/ZenModeProvider";
+import { useContext } from "react";
 
 export default function RespiracaoLayout({ children }: { children: React.ReactNode }) {
+  const { isZenMode } = useContext(ZenModeContext);
+
   return (
     <>
       {children}
-      <FloatingAudioPlayer />
+      {!isZenMode && <FloatingAudioPlayer />}
     </>
   );
 }
